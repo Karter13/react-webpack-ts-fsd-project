01e99a92b190fd81d23c833ae8ef59a909a15220
Karter13 <KarMiH1975@mail.ru>
2023-09-29 16:55:00 +0300

    fix MainPage.stories.tsx

--- src/pages/MainPage/ui/MainPage.stories.tsx
+++ src/pages/MainPage/ui/MainPage.stories.tsx
@@ -13,7 +13,7 @@ export default {
     },
 } as ComponentMeta<typeof MainPage>;
 
-const Template: ComponentStory<typeof MainPage> = (args) => <MainPage {...args} />;
+const Template: ComponentStory<typeof MainPage> = (args) => <MainPage {...(args ?? {})} />;
 
 export const MainPageClear = Template.bind({});
 MainPageClear.args = {};
