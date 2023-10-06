c4a4e4e767fc11ff5f54d9be842ba95613f36db4
Karter13 <KarMiH1975@mail.ru>
2023-10-06 12:46:17 +0300

    fix MainPage

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,25 +1,12 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { useTranslation } from 'react-i18next';
-import { Counter } from 'entities/Counter';
-import { Input } from 'shared/ui/Input/Input';
 
 const MainPage = () => {
     const { t } = useTranslation('main');
 
-    const [value, setValue] = useState('');
-
-    const onchange = (value: string) => {
-        setValue(value);
-    };
-
     return (
         <div>
             {t('Главная страница')}
-            <Input
-                value={value}
-                onChange={onchange}
-                placeholder="Введите имя"
-            />
         </div>
     );
 };
