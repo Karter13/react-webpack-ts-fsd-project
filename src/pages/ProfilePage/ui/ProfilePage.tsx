daf710e14f6b284100968c7977c4c03d1827f205
Karter13 <KarMiH1975@mail.ru>
2023-10-20 13:19:12 +0300

    fix tests config

--- src/pages/ProfilePage/ui/ProfilePage.tsx
+++ src/pages/ProfilePage/ui/ProfilePage.tsx
@@ -3,7 +3,7 @@ import { useTranslation } from 'react-i18next';
 import { DynamicModuleLoader, ReducersList } from 'shared/lib/components/DynamicModuleLoader/DynamicModuleLoader';
 import { useAppDispatch } from 'shared/lib/hooks/useDispatch/useDispatch';
 import { useEffect } from 'react';
-import { fetchProfileData, ProfileCard, profileReducer } from 'entities/Profile';
+import { fetchProfileData, ProfileCard, profileReducer } from '../../../entities/Profile';
 
 const reducers: ReducersList = {
     profile: profileReducer,
